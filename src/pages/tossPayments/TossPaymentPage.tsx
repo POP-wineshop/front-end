61556e8a32f7988dce54a6b721c07d2fb7e6f33e
dilkusha27 <dilkusha27@gmail.com>
2025-07-07 23:14:15 +0900

    refactor: 결제 실패 페이지 라우트 재설정

--- src/pages/tossPayments/TossPaymentPage.tsx
+++ src/pages/tossPayments/TossPaymentPage.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import { WidgetCheckout } from '@/pages/tossPayments/widget/WidgetCheckout';
 import { WidgetSuccess } from '@/pages/tossPayments/widget/WidgetSuccess';
-import { FailPage } from '@/pages/tossPayments/Fail';
+import { WidgetFail } from '@/pages/tossPayments/widget/WidgetFail';
 import '@/styles/tossPayments/TossPaymentPage.css';
 
 export default function TossPaymentPage() {
@@ -9,7 +9,7 @@ export default function TossPaymentPage() {
     <Routes>
       <Route path="/" element={<WidgetCheckout />} />
       <Route path="success" element={<WidgetSuccess />} />
-      <Route path="fail" element={<FailPage />} />
+      <Route path="fail" element={<WidgetFail />} />
     </Routes>
   );
 }
