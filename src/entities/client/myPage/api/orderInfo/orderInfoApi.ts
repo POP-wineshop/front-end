6360f88444cb2ba5df03d5fdc91f882f54bcaab9
dilkusha27 <dilkusha27@gmail.com>
2025-09-01 23:58:22 +0900

    refactor: 주문 조회 및 취소 API 호출 기능 관련 폴더 구조 변경

---
+++ src/entities/client/myPage/api/orderInfo/orderInfoApi.ts
@@ -0,0 +1,33 @@
+import { setOrderItemList } from '../../model/orderInfo';
+
+export const readUserOrders = () => {
+  fetch(`http://localhost:8080/api/orders/my`, {
+    headers: {
+      Authorization: `${localStorage.getItem('Access Token')}`,
+    },
+  })
+    .then((res) => res.json())
+    .then((jsonRes) => {
+      setOrderItemList(jsonRes.data);
+    });
+};
+
+// 주문 취소 API 호출
+export const handleRequestOrderCancel = (
+  e: React.MouseEvent<HTMLButtonElement>,
+  orderId: number
+) => {
+  e.stopPropagation();
+  fetch(`http://localhost:8080/api/orders/${orderId}/cancel`, {
+    method: 'DELETE',
+    headers: {
+      Authorization: `${localStorage.getItem('Access Token')}`,
+    },
+  })
+    .then((res) => res.json())
+    .then((jsonRes) => console.log(`주문 취소 요청 성공: ${jsonRes}`))
+    .catch((err) => {
+      console.error(`주문 취소 요청 실패: ${err}`);
+      alert(`주문 취소 요청 실패 ㅠ: ${err}`);
+    });
+};
