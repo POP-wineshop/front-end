eb9ffa0eb4e4dac7fb4013c9d0650973ae6a9dda
dilkusha27 <dilkusha27@gmail.com>
2025-09-04 22:45:50 +0900

    refactor: 좋아요 아이템 선택자 함수 추가

--- src/entities/client/myPage/model/likesInfo/selector.ts
+++ src/entities/client/myPage/model/likesInfo/selector.ts
@@ -1,1 +1,15 @@
 import { RootState } from '@/shared/store';
+import { LikesWineItemExample } from './likesInfoTypes';
+
+// 좋아요 아이템 리스트 전체 선택자
+export const selectLikesItemList = (state: RootState): LikesWineItemExample[] =>
+  state.likesInfo.likesItems;
+
+// 특정 좋아요 아이템 id로 선택
+export const selectLikesWineItemById = (
+  state: RootState,
+  id: number
+): LikesWineItemExample | undefined =>
+  state.likesInfo.likesItems.find(
+    (item: LikesWineItemExample) => item.id === id
+  );
