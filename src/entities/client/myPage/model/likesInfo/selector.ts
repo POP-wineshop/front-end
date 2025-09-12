046ebbb1cef0bdb7b38923ae76358bb6275d09ca
dilkusha27 <dilkusha27@gmail.com>
2025-09-12 18:08:39 +0900

    refactor: 주문 목록 조회 및 주문 취소 요청 기능 추가, 상태 관리 개선

--- src/entities/client/myPage/model/likesInfo/selector.ts
+++ src/entities/client/myPage/model/likesInfo/selector.ts
@@ -3,13 +3,13 @@ import { LikesWineItemExample } from './likesInfoTypes';
 
 // 좋아요 아이템 리스트 전체 선택자
 export const selectLikesItemList = (state: RootState): LikesWineItemExample[] =>
-  state.likesInfo.likesItems;
+  state.likesInfo.likesItemList;
 
 // 특정 좋아요 아이템 id로 선택
 export const selectLikesWineItemById = (
   state: RootState,
   id: number
 ): LikesWineItemExample | undefined =>
-  state.likesInfo.likesItems.find(
+  state.likesInfo.likesItemList.find(
     (item: LikesWineItemExample) => item.id === id
   );
