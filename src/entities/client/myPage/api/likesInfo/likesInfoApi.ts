8be655da70ba081525290c4f1e6659eb4f60ee39
dilkusha27 <dilkusha27@gmail.com>
2025-09-12 18:08:10 +0900

    refactor: readLikesItemList 함수에서 반환 데이터 수정

--- src/entities/client/myPage/api/likesInfo/likesInfoApi.ts
+++ src/entities/client/myPage/api/likesInfo/likesInfoApi.ts
@@ -1,5 +1,5 @@
 export const readLikesItemList = async () => {
   const response = await fetch(`http://localhost:8080/api/wines`);
   const data = await response.json();
-  return data.slice(0, 5);
+  return data;
 };
