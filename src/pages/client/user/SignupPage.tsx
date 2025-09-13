f34a3181e05684719de3f310722c4bc0f71c47ff
dilkusha27 <dilkusha27@gmail.com>
2025-09-13 21:59:47 +0900

    refactor: 로그인 및 회원가입 페이지에서 로직을 커스텀 훅으로 분리

--- src/pages/client/user/SignupPage.tsx
+++ src/pages/client/user/SignupPage.tsx
@@ -1,24 +1,16 @@
-import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import SignupForm from '@/entities/client/user/ui/SignupForm';
-import { signupUser } from '@/entities/client/user/api/signupUser';
+import { SignupForm } from 'entities/client/user/ui';
 
 const SignupPage = () => {
-  const navigate = useNavigate();
-
   return (
-    <div className="m-auto w-full py-32">
-      <SignupForm />
-      <div className="text-center mt-4">
-        <span className="text-sm text-gray-600">이미 계정이 있으신가요? </span>
-        <Link
-          to="/login"
-          className="text-sm text-[#6A1B1A] hover:underline font-medium"
-        >
-          로그인
-        </Link>
+    <>
+      <div
+        className="m-auto w-full py-32"
+        // 스크린 높이 - 헤더 높이 (120px)
+        // style={{ minHeight: 'calc(100vh - 120px)' }}
+      >
+        <SignupForm />
       </div>
-    </div>
+    </>
   );
 };
 
