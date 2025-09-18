9c8f79061b3876136b567f275a15f00e8fd5ac53
dilkusha27 <dilkusha27@gmail.com>
2025-09-18 21:25:19 +0900

    refactor: 와인 관련 컴포넌트 경로 이동

---
+++ src/entities/client/wine/ui/WineInfo/WineDetailSection.tsx
@@ -0,0 +1,22 @@
+import { getWineDetailSectionFields, WineData } from '../../model';
+
+const WineDetailSection = ({ wineInfoData }: { wineInfoData: WineData }) => {
+  return (
+    <div className="wine-description-bottom py-12 px-6 bg-white flex flex-col gap-10">
+      {getWineDetailSectionFields().map(({ key, value }) => (
+        <div
+          key={key}
+          className="w-full max-w-3xl mx-auto flex flex-col items-start gap-4"
+        >
+          <h3 className="text-lg font-semibold text-[#6A1B1A]">{key}</h3>
+          <hr className="w-full border-t border-gray-300" />
+          <p className="text-gray-700 text-sm leading-relaxed whitespace-pre-wrap">
+            {value}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default WineDetailSection;
