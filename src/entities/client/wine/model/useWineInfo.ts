c9c76868f813d8dd2e408c3c7e7f8d4d9c2b5ce8
dilkusha27 <dilkusha27@gmail.com>
2025-09-23 21:40:39 +0900

    refactor: useWineInfo 훅을 추가하여 와인 정보 패칭 로직 통합 및 WineInfoPage에서 fetchWineDetail 제거, console.time() 추가

--- src/entities/client/wine/model/useWineInfo.ts
+++ src/entities/client/wine/model/useWineInfo.ts
@@ -1,9 +1,27 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { WineData } from './wineTypes';
-import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
 import { createCartItemData, createInstantOrderData } from './wineUtils';
 import { addToCart } from '../../cart/api/cartApi';
+import { fetchWineInfo } from '../api/wineApi';
+
+export const useWineInfo = () => {
+  const [wineInfo, setWineInfo] = useState<WineData | null>(null);
+
+  useEffect(() => {
+    console.time('와인 정보 데이터 패칭');
+    fetchWineInfo()
+      .then((res) => {
+        console.timeEnd('와인 정보 데이터 패칭');
+        setWineInfo(res);
+      })
+      .catch((error) => {
+        console.timeEnd('와인 정보 데이터 패칭');
+        console.error('와인 정보 조회 실패', error);
+      });
+  }, []);
+
+  return { wineInfo };
+};
 
 export function useWineQuantity() {
   const [quantity, setQuantity] = useState<number>(1);
@@ -11,18 +29,15 @@ export function useWineQuantity() {
 }
 
 export function useWineOrder(wineData: WineData, quantity: number) {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-
   const handleAddToCart = () => {
-    const cartItemData = createCartItemData(wineData, quantity);
+    const cartItemData = createCartItemData(wineData.id, quantity);
     addToCart(cartItemData);
   };
 
   // TODO: 장바구니에 이미 포함되어 있을 시 이미 포함되어있다는 알림 처리
 
   const handleInstantOrder = () => {
-    const instantOrderData = createInstantOrderData(wineData, quantity);
+    const instantOrderData = createInstantOrderData(wineData.id, quantity);
     addToCart(instantOrderData);
     // TODO: 이거 왜 addToCart로 되어있는지 파악하기
   };
--- src/pages/client/wine/WineInfoPage.tsx
+++ src/pages/client/wine/WineInfoPage.tsx
@@ -1,27 +1,28 @@
 import { useEffect, useState } from 'react';
 import { WineData } from '@/entities/client/wine/model/wineTypes';
 import { useParams } from 'react-router-dom';
-import { fetchWineDetail } from '@/entities/client/wine/api/wineApi';
+import { useWineInfo } from '@/entities/client/wine/model/useWineInfo';
 import WineSummarySection from '@/entities/client/wine/ui/WineInfo/WineSummarySection';
 import WineDetailSection from '@/entities/client/wine/ui/WineInfo/WineDetailSection';
 
 const WineInfoPage = () => {
-  const [wineInfoData, setWineInfoData] = useState<WineData>();
+  const { wineInfo } = useWineInfo();
   const { wineId } = useParams();
 
   useEffect(() => {
-    fetchWineDetail().then((res) => {
-      setWineInfoData(res);
-    });
+    console.time('와인 정보 페이지 렌더링');
+    return () => {
+      console.timeEnd('와인 정보 페이지 렌더링');
+    };
   }, [wineId]);
 
   return (
     <>
       <div className="wine-description w-full min-w-[900px]">
-        {wineInfoData && (
+        {wineInfo && (
           <>
-            <WineSummarySection wineInfoData={wineInfoData} />
-            <WineDetailSection wineInfoData={wineInfoData} />
+            <WineSummarySection wineInfoData={wineInfo} />
+            <WineDetailSection wineInfoData={wineInfo} />
           </>
         )}
       </div>
