1790db487256e7f4f8223bd5f37550681248607c
dilkusha27 <dilkusha27@gmail.com>
2025-09-25 15:03:36 +0900

    refactor: API 호출 시 응답 속도 측정 로그 추가

--- src/entities/client/cart/api/cartApi.ts
+++ src/entities/client/cart/api/cartApi.ts
@@ -1,19 +1,22 @@
-import { CartItemDataRes } from '../model/cartTypes';
-import { CartItemDataReq } from '../../wine/model';
+import { CartItemDataReq } from '../model/cartTypes';
 
 const API_BASE_URL = 'http://localhost:8080/api';
 
-export const addToCart = async (cartItemData: any) => {
+export const addToCart = async (cartItem: CartItemDataReq) => {
+  console.time('addToCart');
   const response = await fetch(`http://localhost:8080/api/carts`, {
     method: 'POST',
     headers: {
       Authorization: `${localStorage.getItem('Access Token')}`,
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(cartItemData),
+    body: JSON.stringify(cartItem),
   });
 
   const jsonRes = await response.json();
+  console.timeEnd('addToCart');
+  console.log('addToCart 응답:', jsonRes);
+
   if (jsonRes.data.length === 0) {
     throw new Error('장바구니 추가 항목이 비어 있음. 다시 시도하세요.');
   }
@@ -23,6 +26,7 @@ export const addToCart = async (cartItemData: any) => {
 
 // 장바구니 아이템 목록 조회
 export const fetchCartItems = async () => {
+  console.time('fetchCartItems');
   const response = await fetch(`${API_BASE_URL}/carts`, {
     headers: {
       Authorization: `${localStorage.getItem('Access Token')}`,
@@ -30,6 +34,8 @@ export const fetchCartItems = async () => {
   });
 
   const jsonRes = await response.json();
+  console.timeEnd('fetchCartItems');
+  console.log('fetchCartItems 응답:', jsonRes);
 
   if (!response.ok) {
     throw new Error(jsonRes.message);
@@ -43,6 +49,7 @@ export const updateCartItemQuantity = async (
   cartItemId: number,
   quantity: number
 ) => {
+  console.time('updateCartItemQuantity');
   const response = await fetch(`${API_BASE_URL}/carts/${cartItemId}`, {
     method: 'PATCH',
     headers: {
@@ -52,15 +59,20 @@ export const updateCartItemQuantity = async (
     body: JSON.stringify({ quantity }),
   });
 
+  const jsonRes = await response.json();
+  console.timeEnd('updateCartItemQuantity');
+  console.log('updateCartItemQuantity 응답:', jsonRes);
+
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}`);
   }
 
-  return response.json();
+  return jsonRes.data;
 };
 
 // 장바구니 아이템 삭제
 export const deleteCartItem = async (cartItemId: number) => {
+  console.time('deleteCartItem');
   const response = await fetch(`${API_BASE_URL}/carts/${cartItemId}`, {
     method: 'DELETE',
     headers: {
@@ -68,9 +80,13 @@ export const deleteCartItem = async (cartItemId: number) => {
     },
   });
 
+  const jsonRes = await response.json();
+  console.timeEnd('deleteCartItem');
+  console.log('deleteCartItem 응답:', jsonRes);
+
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}`);
   }
 
-  return response.json();
+  return jsonRes.data;
 };
