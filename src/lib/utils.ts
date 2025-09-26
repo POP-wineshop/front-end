da24bccd0b343c1d0d6f096c98efa47d4eb5f82d
dilkusha27 <dilkusha27@gmail.com>
2025-09-26 17:24:18 +0900

    refactor: 코드 스타일 개선 및 cn 함수에 대한 주석 추가

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+/**
+ * cn = "class names"의 약자
+ * 여러 TailwindCSS 클래스명을 조건부로 합치고, 중복/충돌되는 클래스를 자동으로 정리합니다.
+ *
+ * 예시:
+ *   cn("px-4", isActive && "bg-blue-500", "px-2")
+ *   // 결과: "bg-blue-500 px-2" (중복 px-4 제거)
+ */
