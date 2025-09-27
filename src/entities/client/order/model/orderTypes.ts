9fa7fd28f72d084c3e896493155d35b9d208bc93
dilkusha27 <dilkusha27@gmail.com>
2025-09-27 23:58:54 +0900

    refactor: 주문 데이터 타입을 type -> interface로 변경하여 일관성 개선

--- src/entities/client/order/model/orderTypes.ts
+++ src/entities/client/order/model/orderTypes.ts
@@ -1,21 +1,23 @@
-export type OrderReqData = {
+// 일단 파생되는 타입보다는 단순한 타입 위주로 작성할 것이므로 interface로 변경
+
+export interface OrderDataReq {
   wineId: number;
   quantity: number;
-};
+}
 
-export type OrderResData = {
+export interface OrderDataRes {
   orderId: number;
   tossOrderId: string;
   orderStatus: string;
   orderItems: OrderWineItem[];
   totalPrice: number;
-};
+}
 
-export type OrderWineItem = {
+export interface OrderWineItem {
   wineId: number;
   wineNameKor: string;
   winePrice: number;
   orderedQuantity: number;
   orderedPrice: number;
   wineImageUrl: string;
-};
+}
